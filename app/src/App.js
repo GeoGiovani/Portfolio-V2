19565259c6c2c50dfaea94aa408a328aa408bc97
George Giovanis <georgedgiovani.dev@gmail.com>
2020-05-11 18:36:41 -0700

    Edit: Created React App
    Signed-off-by: George Giovanis <georgedgiovani.dev@gmail.com>

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>
+        George Giovanis' Personal Website V2
+      </p>
+    </div>
+  );
+}
+
+export default App;
