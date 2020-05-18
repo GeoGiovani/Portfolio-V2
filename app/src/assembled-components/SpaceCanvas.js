86835362c14e57e9264388ef16408b11b2f0379b
George Giovanis <georgedgiovani.dev@gmail.com>
2020-05-18 16:52:49 -0700

    Edit: Committing progress on SpaceCanvas
    Signed-off-by: George Giovanis <georgedgiovani.dev@gmail.com>

--- app/src/assembled-components/SpaceCanvas.js
+++ app/src/assembled-components/SpaceCanvas.js
@@ -1,41 +1,38 @@
 import React from 'react'
 import {PtsCanvas} from 'react-pts-canvas'
-import {CanvasSpace, Form, Line, Create, Group, Pt} from 'pts/dist/es5'
+import {Line, Create, Group, Pt} from 'pts/dist/es5'
 
 class ShootingStars extends PtsCanvas {
 
   start (bound, space) {
-    this.pts = Create.distributeRandom( space.innerBound, 75 );
+    this.pts = Create.distributeRandom( space.innerBound, 125 );
     this.nw = new Pt(0,0);
     this.ne = new Pt(space.width, 0);
+    this.sw = new Pt(0, space.height);
+    this.se = new Pt(space.width, space.height);
     this.mouse = space.center;
     this.colors = ["#FF3F8E", "#04C2C9", "#2E55C1"];
   }
 
   animate (time, ftime, space) {
-    // make a line and turn it into an "op" (see the guide on Op for more)
-    let perpend = new Group( this.nw, this.ne ).op( Line.perpendicularFromPt );
-    this.pts.rotate2D( 0.0005, space.center );
-
+    // Generate a Line object along bottom axis
+    let perpends = new Group( this.sw, this.se ).op( Line.perpendicularFromPt );
+    // Rotate all points
+    this.pts.rotate2D( 0.0003, space.center );
+    // Draw perpendicular lines to each point
     this.pts.forEach( (p, i) => {
-      // for each point, find the perpendicular to the line
-      let lp = perpend( p );
-      var ratio = 0.3
-      this.form.stroke(`rgba(255,255,255,${ratio}`, ratio*2).line( [ p, lp ] );
+      let lp = perpends( p );
+      var ratio = Math.min( 0.6, 1 - lp.$subtract(p).magnitude()/(space.size.x/2.25) );
+      let fadeColors = ['rgba(255,63,142', 'rgba(4,194,201', 'rgba(46,85,193'];
+      this.form.stroke(`${fadeColors[i%3]},${ratio})`, ratio*2).line( [ p, lp ] );
       this.form.fillOnly( this.colors[i%3] ).point( p, 1 );
     });
-  }
-
-  action (type, px, py, event) {
-    if (type=="move") {
-      this.mouse = [parseFloat(px),parseFloat(py)];
-    }
   }
 }
 
 function SpaceCanvas(props) {
   return (
-    <ShootingStars background="#000" />
+    <ShootingStars background="#000" style={{height: '100%', width: '100%', position: 'absolute', top: 0, left: 0}}/>
   );
 }
 
