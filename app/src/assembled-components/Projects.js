401c2696dbb8ffbe186a71b90fe05aa8fa0be476
George Giovanis <georgedgiovani.dev@gmail.com>
2020-05-26 19:39:14 -0700

    Edit: added SwiftCalc grid stub; grid hover transitions
    Signed-off-by: George Giovanis <georgedgiovani.dev@gmail.com>

--- app/src/assembled-components/Projects.js
+++ app/src/assembled-components/Projects.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import Banner from '../components/Banner.js'
+import SplitPaneImage from '../components/SplitPaneImage.js'
 import '../styles/index.css';
 
 function Projects() {
   return (
-    <div id="Projects" className="w-screen h-screen bg-gray-100">
+    <div id="Projects" className="w-screen h-screen bg-custom-black-800 ">
       
       <Banner 
         header={
-          <div className="banner-content text-custom-black-900">
+          <div className="banner-content text-gray-300">
             <p className="underline-small-projects">
               Projects
             </p>
           </div>}
-        bannerStyle="profile-banner"
-        headerStyle="profile-banner-header"
+        bannerStyle="profile-banner bg-custom-black-800"
+        headerStyle="profile-banner-header bg-custom-black-800"
       />
 
-      <div className="project-content flex justify-center items-center box-border w-screen h-auto mt-12 font-roboto font-custom-black-900 text-5xl">
-        <div className="project-grid flex items-center w-p80 h-142">
-          <div className="grid grid-rows-2 grid-cols-3 gap-1 box-border w-full h-full">
+      <div className="project-content">
+        <div className="project-grid-wrapper">
+          <div className="project-grid">
 
-            {/* SwiftCalc Grid */}
-            <div class="grid-card-SwiftCalc row-span-2 col-span-1 bg-custom-green-400 bg-opacity-50 flex items-center justify-center shadow-xl relative">
-              SwiftCalc
+            <div class="grid-card-SwiftCalc">
+
+              <div class="grid-content bg-teal-100 rounded-md">
+                  <div class="grid-content-overlay bg-custom-black-900 bg-opacity-75"></div>
+
+                  <SplitPaneImage 
+                    src={require("../media/SwiftCalc.png")}
+                    alt="SwiftCalc"
+                    outerCardStyle="box-border h-full overflow-hidden"
+                    imageStyle=""
+                  />
+
+                  <div class="grid-content-details slide-bottom">
+                    <h3>SwiftCalc</h3>
+                    <p>A minimalist calculator</p>
+                  </div>
+              </div>
+
             </div>
 
             <div class="row-span-1 col-span-1 bg-custom-red-500 flex items-center justify-center">
