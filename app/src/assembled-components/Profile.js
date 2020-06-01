b72183585bd3a8a1af35c15da3b9af5bf68f36c2
George Giovanis <georgedgiovani.dev@gmail.com>
2020-06-01 13:35:55 -0700

    Edit: changed Profile component color scheme to dark mode; replaced contact button to link
    Signed-off-by: George Giovanis <georgedgiovani.dev@gmail.com>

--- app/src/assembled-components/Profile.js
+++ app/src/assembled-components/Profile.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Banner from '../components/Banner.js'
 import SplitPaneImage from '../components/SplitPaneImage.js'
-import LinkButton from '../components/LinkButton.js'
 import '../styles/index.css';
 
 
@@ -11,12 +10,12 @@ function Profile() {
 
       <Banner 
         header={
-          <div className="banner-content text-gray-900">
+          <div className="banner-content text-gray-300">
             <p className="underline-small">
               Profile
             </p>
           </div>}
-        bannerStyle="profile-banner bg-gray-100"
+        bannerStyle="profile-banner"
         headerStyle="profile-banner-header"
       />
 
@@ -26,30 +25,27 @@ function Profile() {
               <p className="mb-4">
                 Hello! I'm a third-year Computer Science student
                 based in Vancouver, BC. I currently study at&nbsp;
-                <a className="profile-link text-custom-purple-400 font-bold" 
+                <a className="profile-link text-custom-yellow-300 font-bold" 
                 href="https://www.sfu.ca/">Simon Fraser University
                 </a> and volunteer in the&nbsp;
-                <a className="profile-link text-custom-purple-400 font-bold" 
+                <a className="profile-link text-custom-yellow-300 font-bold" 
                 href="https://sfumars.com/people/george-giovanis/"> 
-                MARS Laboratory</a>.
+                MARS Laboratory.</a>
               </p>
               <p className="mb-4">
                 I love creating intuitive web and mobile applications. My 
                 goal is to provide users with fast, enjoyable, and reliable
-                software. To do this, I develop using proven 
+                software. To do this, I develop using modern 
                 software-engineering methods.
               </p>
               <p className="mb-8">
                 I am currently seeking a software engineering or full-stack
-                developer co-op/internship.
+                developer co-op/internship.&nbsp;
+                <a className="profile-link text-custom-red-500 font-bold"
+                href="mailto:georgedgiovani.dev@gmail.com/"> 
+                Let's get in touch.</a>
               </p>
-              <div className="profile-button">
-                <LinkButton
-                  link="mailto:georgedgiovani.dev@gmail.com"
-                  content="Contact Me"
-                  buttonStyle="btn-contact"
-                />
-              </div>
+
             </div>
           </div>
 
