6506800eb2d4fdd3fdd858ac78fbbd2136b5b2e5
George Giovanis <georgedgiovani.dev@gmail.com>
2020-06-02 23:37:03 -0700

    Edit: minor code style refactor for Projects component
    Signed-off-by: George Giovanis <georgedgiovani.dev@gmail.com>

--- app/src/assembled-components/Projects.js
+++ app/src/assembled-components/Projects.js
@@ -18,111 +18,112 @@ function Projects() {
             <p className="underline-small-projects">
               Projects
             </p>
-          </div>}
+          </div>
+        }
       />
 
       <div className="project-content bg-custom-black-800">
 
         <PortfolioGrid 
           wrapperStyle="project-grid-wrapper"
-          content={ <div className="project-grid">
+          content={
+            <div className="project-grid">
 
-
-            <GridComponent
-              wrapperStyle="grid-card-SwiftCalc"
-              content={ <div class="grid-content bg-custom-yellow-300">
-
-                <div class="grid-content-overlay"></div>
-                <SplitPaneImage 
-                  src={require("../media/SwiftCalc.png")}
-                  alt="SwiftCalc"
-                  outerCardStyle="box-border h-full overflow-hidden"
-                  imageStyle=""
-                />
-                <div class="grid-content-details slide-down">
-                  <h3>SwiftCalc</h3>
-                  <p>A minimalist calculator for iOS</p>
-                </div>
-
-              </div> }
-            />
-
-            <GridComponent 
-              wrapperStyle="grid-card-MathBender"
-              content={ <div class="grid-content bg-custom-green-400">
-                
-                <div class="grid-content-overlay"></div>
-                <SplitPaneImage 
-                  src={require("../media/mathBender.gif")}
-                  alt="SwiftCalc"
-                  outerCardStyle="box-border h-full flex items-end overflow-auto"
-                  imageStyle=""
-                />
-                <div class="grid-content-details slide-left">
-                  <h3>MathBender</h3>
-                  <p>A bot that queries Wolfram|Alpha</p>
-                </div>
-              
-              </div> }
-            />
-
-            <GridComponent
-              wrapperStyle="row-span-1 col-span-1"
-              content={ <div class="grid-content bg-custom-red-500">
-
-                <div class="grid-content-overlay"></div>
-                {/* TODO: Showcase SFU work */}
-                <div class="grid-content-details slide-left">
-                  <h3>School Project</h3>
-                  <p>A web-based survival game</p>
-                </div>
-
-              </div> }
-            />
-
-            <GridComponent
-              wrapperStyle="grid-card-detector"
-              content={ <div class="grid-content bg-blue-200">
-
-                <div class="grid-content-overlay"></div>
-                <SplitPaneImage 
-                  src={require("../media/STImg.png")}
-                  alt="SwiftCalc"
-                  outerCardStyle="box-border h-auto flex items-end overflow-auto"
-                  imageStyle="h-40"
-                />
-                <div class="grid-content-details slide-left">
-                  <h3>School Project</h3>
-                  <p>A video Transition Detector</p>
-                </div>
-
-              </div> }
-            />
-            
-            <GridComponent
-              wrapperStyle="grid-card-Portfolio"
-              content={ <div class="grid-content">
-                
-                <div class="grid-content-overlay z-10"></div>
-                <div class="browser-mockup">
-                  <SpaceCanvasMini
-                      bgColor="#101010"
-                      style={{height: '101%', width: '101%'}}
+              <GridComponent
+                wrapperStyle="grid-card-SwiftCalc"
+                content={ 
+                  <div class="grid-content bg-custom-yellow-300">
+                    <div class="grid-content-overlay"></div>
+                    <SplitPaneImage 
+                      src={require("../media/SwiftCalc.png")}
+                      alt="SwiftCalc"
+                      outerCardStyle="box-border h-full overflow-hidden"
+                      imageStyle=""
                     />
-                </div>
-                <div class="grid-content-details details-thin z-20 slide-up">
-                  <h3>Portfolio V2</h3>
-                  <p>My interactive portfolio</p>
-                </div>
+                    <div class="grid-content-details slide-down">
+                      <h3>SwiftCalc</h3>
+                      <p>A minimalist calculator for iOS</p>
+                    </div>
+                  </div> 
+                }
+              />
 
-              </div> }
-            />
+              <GridComponent 
+                wrapperStyle="grid-card-MathBender"
+                content={ 
+                  <div class="grid-content bg-custom-green-400">
+                    <div class="grid-content-overlay"></div>
+                    <SplitPaneImage 
+                      src={require("../media/mathBender.gif")}
+                      alt="SwiftCalc"
+                      outerCardStyle="box-border h-full flex items-end"
+                      imageStyle=""
+                    />
+                    <div class="grid-content-details slide-left">
+                      <h3>MathBender</h3>
+                      <p>A bot that queries Wolfram|Alpha</p>
+                    </div>
+                  </div> 
+                }
+              />
 
+              <GridComponent
+                wrapperStyle="row-span-1 col-span-1"
+                content={ 
+                  <div class="grid-content bg-custom-red-500">
+                    <div class="grid-content-overlay"></div>
+                    {/* TODO: Showcase SFU work */}
+                    <div class="grid-content-details slide-left">
+                      <h3>School Project</h3>
+                      <p>A web-based survival game</p>
+                    </div>
+                  </div> 
+                }
+              />
 
-          </div> }   
-        />{/* End of PortfolioGrid */}
+              <GridComponent
+                wrapperStyle="grid-card-detector"
+                content={ 
+                  <div class="grid-content bg-blue-200">
+                    <div class="grid-content-overlay"></div>
+                    <SplitPaneImage 
+                      src={require("../media/STImg.png")}
+                      alt="SwiftCalc"
+                      outerCardStyle="box-border h-auto flex items-end"
+                      imageStyle="h-40"
+                    />
+                    <div class="grid-content-details slide-left">
+                      <h3>School Project</h3>
+                      <p>A video Transition Detector</p>
+                    </div>
+                  </div> 
+                }
+              />
+              
+              <GridComponent
+                wrapperStyle="grid-card-Portfolio"
+                content={ 
+                  <div class="grid-content">
+                    <div class="grid-content-overlay z-10"></div>
+                    <div class="browser-mockup">
+                      <SpaceCanvasMini
+                          bgColor="#101010"
+                          style={{height: '101%', width: '101%'}}
+                        />
+                    </div>
+                    <div class="grid-content-details grid-thin z-20 slide-up">
+                      <h3>Portfolio V2</h3>
+                      <p>My interactive portfolio</p>
+                    </div>
+                  </div> 
+                }
+              />
+
+            </div> 
+          }   
+        />    {/* End of PortfolioGrid */}
         
-      </div>{/* End of project-content */}
+      </div>  {/* End of project-content */}
     </div> 
   );
 }
