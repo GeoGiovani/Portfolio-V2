a61b6901a416b1c7a3e313592adeeb46a5ff3529
George Giovanis <georgedgiovani.dev@gmail.com>
2020-06-07 21:17:06 -0700

    Edit: modified SpaceCanvas component to increase performance on Safari
    Signed-off-by: George Giovanis <georgedgiovani.dev@gmail.com>

--- app/src/components/SpaceCanvas.js
+++ app/src/components/SpaceCanvas.js
@@ -1,33 +1,71 @@
 import React from 'react'
 import {PtsCanvas} from 'react-pts-canvas'
-import {Line, Create, Group, Pt} from 'pts/dist/es5'
+import {Line, Create, Group, Pt, Bound} from 'pts'
 
 class ShootingStars extends PtsCanvas {
 
   start (bound, space) {
-    this.pts = Create.distributeRandom( space.innerBound, (105*space.size.x/space.size.y)  );
-    this.sw = new Pt(0, space.height);
-    this.se = new Pt(space.width, space.height);
-    this.mouse = space.center;
-    this.colors = ["#FF3F8E", "#04C2C9", "#fb5"];
+
+    this.height = space.height;
+    this.width = space.width;
+    this.center = space.center;
+
+    this.unw = new Pt(0,0);
+    this.use = new Pt(this.width, this.height/3);
+
+    this.mnw = new Pt(0,this.height/3);
+    this.mse = new Pt(this.width, 2*this.height/3);
+
+    this.lnw = new Pt(0, 2*this.height/3);
+    this.lse = new Pt(this.width, this.height)
+    this.lsw = new Pt(0, this.height)
+
+    this.upperBound = new Bound(this.unw, this.use)
+    this.middleBound = new Bound(this.mnw, this.mse)
+    this.lowerBound = new Bound(this.lnw, this.lse)
+
+    this.numPts = Math.floor((30*space.size.x/space.size.y))
+
+    this.upperPts = Create.distributeRandom(this.upperBound, this.numPts);
+    this.middlePts = Create.distributeRandom(this.middleBound, this.numPts);
+    this.lowerPts = Create.distributeRandom(this.lowerBound, this.numPts);
+
+    this.perpends = new Group (this.lsw, this.lse).op(Line.perpendicularFromPt);
+    
+    this.ptColors = ["#FF3F8E", "#04C2C9", "#fb5"];
+    this.lnColors = ['rgba(255,63,142', 'rgba(4,194,201', 'rgba(255,187,85'];
+  }
+  
+  animate (time, ftime, space) {
+
+    // Rotate all points
+    this.upperPts.rotate2D( 0.0003, this.center );
+    this.middlePts.rotate2D( 0.0003, this.center );
+    this.lowerPts.rotate2D( 0.0003, this.center );
+
+    // Draw perpendicular lines to each point
+    this.upperPts.forEach ( (p, i) => {
+      let lp = this.perpends(p)
+      this.drawLine(p, i, lp)
+    });
+    this.middlePts.forEach ( (p, i) => {
+      let lp = this.perpends(p)
+      this.drawLine(p, i, lp)
+    });
+    this.lowerPts.forEach ( (p, i) => {
+      let lp = this.perpends(p)
+      this.drawLine(p, i, lp)
+    });
   }
 
-  animate (time, ftime, space) {
-    // Generate a Line object along bottom axis
-    let perpends = new Group( this.sw, this.se ).op( Line.perpendicularFromPt );
-    // Rotate all points
-    this.pts.rotate2D( 0.0003, space.center );
-    // Draw perpendicular lines to each point
-    this.pts.forEach( (p, i) => {
-      let lp = perpends( p );
-      var ratio = Math.min( .7, 1 - lp.$subtract(p).magnitude()/(space.size.x/(space.size.x/space.size.y*1.3)));
+  drawLine(p, i, lp) {
+    var gamma = 1 - ((this.height-p.y)/this.height*1.2)**2;
+    var ratio = Math.min(.8, gamma);
 
-      // Ensure thickness > 0.5 to avoid resolution scaling bugs
-      var thickness = 1; (ratio*2.5 > 1) ? thickness = ratio*2.5 : thickness = .75;
-      let fadeColors = ['rgba(255,63,142', 'rgba(4,194,201', 'rgba(255,187,85'];
-      this.form.stroke(`${fadeColors[i%3]},${ratio})`, thickness).line( [ p, lp ] );
-      this.form.fillOnly( this.colors[i%3] ).point( p, .5 );
-    });
+    // Ensure thickness > 0.5 to avoid resolution scaling bugs
+    var thickness = 1; (ratio*2.5 > 1) ? thickness = ratio*2.5 : thickness = .75;
+    this.form.stroke(`${this.lnColors[i%3]},${ratio})`, thickness).line([p,lp]);
+    this.form.fillOnly( this.ptColors[i%3]).point( p, .75 );
   }
 }
 
