2c52ef9e58b41cdb1360661d255f9b3a3a48c3d2
George Giovanis <georgedgiovani.dev@gmail.com>
2020-06-20 16:41:44 -0700

    Edit: increased font weight on smaller screens
    Signed-off-by: George Giovanis <georgedgiovani.dev@gmail.com>

--- app/src/components/sub-components/Profile/ProfileText.jsx
+++ app/src/components/sub-components/Profile/ProfileText.jsx
@@ -7,10 +7,10 @@ function ProfileText() {
     <div className="profile-text">
 
       <p className="mb-4">
-        {data["Profile"]["paragraph-1-1"]} &nbsp;
+        {data["Profile"]["paragraph-1-1"]}&nbsp;
         <a className="profile-link text-custom-yellow-300 font-bold" 
         href={data["Profile"]["link-1"]}>{data["Profile"]["link-1-text"]}</a> 
-        &nbsp; {data["Profile"]["paragraph-1-2"]} &nbsp;
+        &nbsp;{data["Profile"]["paragraph-1-2"]}&nbsp;
         <a className="profile-link text-custom-green-400 text-opacity-95 font-bold" 
         href={data["Profile"]["link-2"]}>{data["Profile"]["link-2-text"]}</a>
       </p>
@@ -20,7 +20,7 @@ function ProfileText() {
       </p>
 
       <p className="mb-0">
-        {data["Profile"]["paragraph-3"]} &nbsp;
+        {data["Profile"]["paragraph-3"]}&nbsp;
         <a className="profile-link text-custom-red-400 font-bold"
         href={data["Profile"]["link-3"]}>{data["Profile"]["link-3-text"]}</a>
       </p>
